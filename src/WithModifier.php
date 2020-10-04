13fb52e63a87a5d4cfb4a4633a33039a650b4430
creatortsv <creatortsv@yandex.ru>
2020-10-04 18:48:35 +0300

    create factory modifier

--- src/WithModifier.php
+++ src/WithModifier.php
@@ -20,35 +20,6 @@ trait WithModifier
      */
     public static function modify(array $modifiers = []): Builder
     {
-        return static::modifyTo(static::query(), $modifiers);
-    }
-
-    /**
-     * @param Builder|Relation $query
-     * @param ModifierAbstract[] $modifiers
-     * @return Builder
-     */
-    public static function modifyTo($query, array $modifiers = []): Builder
-    {
-        if ($query instanceof Relation) {
-            if (get_class($query->getParent()) !== static::class) {
-                throw new InvalidArgumentException('Relation query builder must be related of the Model class');
-            }
-
-            $query = $query->getQuery();
-        } elseif ($query instanceof Builder) {
-            if (get_class($query->getModel()) !== static::class) {
-                throw new InvalidArgumentException('Query must be query of the Model class');
-            }
-        } else {
-            throw new InvalidArgumentException('Query must be instance of the Relation or the Builder class');
-        }
-
-        return app(Pipeline::class)
-            ->send($query)
-            ->through($modifiers ?: config('modifier.modifiers'))
-            ->then(function ($builder) {
-                return $builder;
-            });
+        return ModifierFactory::modifyTo(self::query(), $modifiers);
     }
 }
