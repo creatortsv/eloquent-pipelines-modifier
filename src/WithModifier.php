5901dd447429f8d78f6173669f761e86daf1b0b5
creatortsv <creatortsv@yandex.ru>
2020-10-04 03:34:25 +0300

    fix aarguments modifyTo method

--- src/WithModifier.php
+++ src/WithModifier.php
@@ -24,19 +24,24 @@ trait WithModifier
     }
 
     /**
+     * @param Builder|Relation $query
      * @param ModifierAbstract[] $modifiers
      * @return Builder
      */
-    public static function modifyTo(Builder $query, array $modifiers = []): Builder
+    public static function modifyTo($query, array $modifiers = []): Builder
     {
-        if ($query instanceof Relation && get_class($query->getParent()) !== static::class) {
-            throw new InvalidArgumentException('Relation query builder must be related of the Model class');
+        if ($query instanceof Relation) {
+            if (get_class($query->getParent()) !== static::class) {
+                throw new InvalidArgumentException('Relation query builder must be related of the Model class');
+            }
+        } else if ($query instanceof Builder) {
+            if (get_class($query->getModel()) !== static::class) {
+                throw new InvalidArgumentException('Query must be query of the Model class');
+            }
+        } else {
+            throw new InvalidArgumentException('Query must be instance of the Relation or the Builder class');
         }
 
-        if (get_class($query->getModel()) !== static::class) {
-            throw new InvalidArgumentException('Query builder must be query of the Model class');
-        }
-        
         return app(Pipeline::class)
             ->send($query)
             ->through($modifiers ?: config('modifier.modifiers'))
