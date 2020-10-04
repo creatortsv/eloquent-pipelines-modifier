390140ae34ef6eedf2bb8b95d59b9c21a9f8cc64
creatortsv <creatortsv@yandex.ru>
2020-10-04 00:17:17 +0000

    add new method

--- src/WithModifier.php
+++ src/WithModifier.php
@@ -4,7 +4,9 @@ namespace Creatortsv\EloquentPipelinesModifier;
 
 use Creatortsv\EloquentPipelinesModifier\Modifiers\ModifierAbstract;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Pipeline\Pipeline;
+use InvalidArgumentException;
 
 /**
  * Trait WithModifier
@@ -18,8 +20,25 @@ trait WithModifier
      */
     public static function modify(array $modifiers = []): Builder
     {
+        return static::modifyTo(static::query(), $modifiers);
+    }
+
+    /**
+     * @param ModifierAbstract[] $modifiers
+     * @return Builder
+     */
+    public static function modifyTo(Builder $query, array $modifiers = []): Builder
+    {
+        if ($query instanceof Relation && get_class($query->getParent()) !== static::class) {
+            throw new InvalidArgumentException('Relation query builder must be related of the Model class');
+        }
+
+        if (get_class($query->getModel()) !== static::class) {
+            throw new InvalidArgumentException('Query builder must be query of the Model class');
+        }
+        
         return app(Pipeline::class)
-            ->send(self::query())
+            ->send($query)
             ->through($modifiers ?: config('modifier.modifiers'))
             ->then(function ($builder) {
                 return $builder;
